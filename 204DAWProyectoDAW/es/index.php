e794de48746d02989af73edbf269ff4f201c704e
daw2 <daw2@IS32WX12>
2024-10-16 13:38:38 +0200

    Commit hecho desde otro ordenador

---
+++ 204DAWProyectoDAW/es/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Alex</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>Welcome to Despliegue de Aplicaciones web, ahora en php</div>        
+    </body>
+    <footer>
+        <p>Cambios realizados el dia 16/10/2024 desde otro ordenador</p>
+    </footer>
+</html>
