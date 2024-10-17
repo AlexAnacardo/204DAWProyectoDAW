b01599e96762a51fb52e14bc464314010f50835d
Alex <alex.asesan@educa.jcyl.es>
2024-10-17 11:29:41 +0200

    Commit 17/10/2024

--- es/index.php
+++ es/index.php
@@ -14,5 +14,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
     </body>
     <footer>
         <p>Cambios hechos el dia 16/10/2024 desde otro ordenador</p>
+        <p><a target="blank" href="doc/Curriculum.pdf">Curriculum</a></p>
+        <p><a target="blank" href="https://github.com/AlexAnacardo">GitHub personal</a></p>
     </footer>
 </html>
